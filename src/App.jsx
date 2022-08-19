293c86c8139a57c260d9c0ad7845a316e67e609f
juan lentino <nuncaparesdecrear@gmail.com>
2022-08-18 19:02:33 -0500

    Inicializando el proyecto, creando carpetas, subcarpetas, archivos de configuracion y instalando todas las dependencias

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = ()=>  {
+    return (
+        <>
+            <div>hola React, estoy de vuelta al ruedo</div>            
+        </>
+    );
+}
+
+export default App;
