813fa06e217fc1c471b87eec28046daa0c0fe381
juan lentino <nuncaparesdecrear@gmail.com>
2022-08-20 10:59:28 -0500

    Creando el componente con las estadísticas del usuario

--- src/containers/Description/index.js
+++ src/containers/Description/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Stack, Typography } from "@mui/material";
+import PaperInformation from '../../components/PaperInformation'
 
 const Description = (props)=> {
     const { userState } = props 
@@ -14,6 +15,7 @@ const Description = (props)=> {
                         : <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</Typography> 
                 }
             </Stack>
+            <PaperInformation userState={userState} />
         </>
     )
 }
